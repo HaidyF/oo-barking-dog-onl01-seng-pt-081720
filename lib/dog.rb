dac981e752ef80f0ada6b27c20f4a8c80e7aad9c
Haidy  Farahat <haidyfarahat@gmail.com>
2020-09-01 04:19:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,13 @@
 # Your code goes here!
 class Dog
-def name= (this_dog)
-  this_dog = name
+def name= (this_dog_name)
+  this_dog_name = new_name
 end
+
+def name
+  this_dog_name
+end
+
  def bark
    @"Woof!"
  end
